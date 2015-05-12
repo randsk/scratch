fedd32c52a275b3a0f030bff12b4227a120a0fa3
randsk <randsk@Kevins-MacBook-Pro.local>
2015-05-12 22:05:23 +0100

    first copy of roamer plugin

---
+++ roamer.js
@@ -0,0 +1,31 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+        ]
+    };
+
+    var device = null;
+    ext._deviceConnected = function(dev) {
+        if(device) return;
+
+        device = dev;
+        // device.open();
+
+        // poller = setInterval(function() {
+        //     rawData = device.read();
+        // }, 20);
+    };
+
+    var hid_info = {type: 'hid', vendor: 0x1040, product: 0x8006};
+    ScratchExtensions.register('Roamer', descriptor, ext, hid_info);
+})({});
